2e19fb6bab569144a2b628f157bab239b0553840
Nelson Griffiths <nelsongriffiths123@gmail.com>
2024-09-05 16:23:18 -0600

    undo deletions

--- polars_trading/_utils.py
+++ polars_trading/_utils.py
@@ -1,1 +1,79 @@
+import inspect
+from functools import wraps
+from typing import Callable
 
+import polars as pl
+
+from polars_trading.typing import FrameType
+
+
+def validate_columns(*column_args: str) -> Callable:
+    """Validate that the specified columns exist in the dataframe.
+
+    Args:
+    ----
+        *column_args: str - The column names to validate.
+
+    Returns:
+    -------
+        Callable
+
+    """
+
+    def decorator(func: Callable) -> Callable:
+        """Decorate validation of columns in a polars DataFrame.
+
+        Args:
+        ----
+            func: Callable
+
+        Returns:
+        -------
+            Callable
+
+        """
+
+        @wraps(func)
+        def wrapper(*args: FrameType, **kwargs: str) -> Callable:
+            """Validate that the specified columns exist in the dataframe.
+
+            Args:
+            ----
+                *args: DataFrameTypes
+                    The first argument must be a polars DataFrame.
+                **kwargs: str
+                    The column names to validate.
+
+            Raises:
+            ------
+                TypeError: If the first argument is not a polars DataFrame.
+                ValueError: If any of the specified columns do not exist.
+
+            Returns:
+            -------
+                Callable
+
+            """
+            # Get the dataframe from args or kwargs
+            df = args[0]
+
+            if not isinstance(df, pl.DataFrame | pl.LazyFrame):
+                raise TypeError("First argument must be a polars DataFrame")
+
+            func_kwargs = {
+                k: v.default
+                for k, v in inspect.signature(func).parameters.items()
+                if v.default is not inspect.Parameter.empty
+            }
+            func_kwargs.update(kwargs)
+            required_columns = [func_kwargs[a] for a in column_args]
+            # Check if all specified columns exist in the dataframe
+            missing_columns = [col for col in required_columns if col not in df.columns]
+            if missing_columns:
+                raise ValueError(f"Missing columns: {', '.join(missing_columns)}")
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
--- tests/test__utils.py
+++ tests/test__utils.py
@@ -1,1 +1,72 @@
+from polars_trading._utils import validate_columns
+import pytest
+import polars as pl
 
+
+def test__validate_columns__valid():
+    @validate_columns("timestamp_col", "price_col", "size_col", "symbol_col")
+    def dummy_func(df, **kwargs):
+        return df
+
+    df = pl.DataFrame(
+        {
+            "ts": [1, 2, 3],
+            "p": [1.0, 2.0, 3.0],
+            "s": [1, 2, 3],
+            "sy": ["A", "B", "C"],
+        }
+    )
+
+    dummy_func(df, timestamp_col="ts", price_col="p", size_col="s", symbol_col="sy")
+
+
+def test__validate_columns__invalid():
+    @validate_columns("timestamp_col", "price_col", "size_col", "symbol_col")
+    def dummy_func(df, **kwargs):
+        return df
+
+    df = pl.DataFrame(
+        {
+            "ts": [1, 2, 3],
+            "p": [1.0, 2.0, 3.0],
+            "s": [1, 2, 3],
+            "sy": ["A", "B", "C"],
+        }
+    )
+
+    with pytest.raises(ValueError):
+        dummy_func(
+            df, timestamp_col="ts", price_col="pr", size_col="s", symbol_col="sy"
+        )
+
+
+def test__validate_columns__df_not_passed():
+    @validate_columns("timestamp_col", "price_col", "size_col", "symbol_col")
+    def dummy_func(df, **kwargs):
+        return df
+
+    df = {
+        "ts": [1, 2, 3],
+        "p": [1.0, 2.0, 3.0],
+        "s": [1, 2, 3],
+        "sy": ["A", "B", "C"],
+    }
+
+    with pytest.raises(TypeError):
+        dummy_func(df, timestamp_col="ts", price_col="p", size_col="s", symbol_col="sy")
+
+
+def test__validate_columns__default_args():
+    @validate_columns("timestamp_col", "price_col", "size_col", "symbol_col")
+    def dummy_func(df, timestamp_col="ts", **kwargs):
+        return df
+
+    df = pl.DataFrame(
+        {
+            "ts": [1, 2, 3],
+            "p": [1.0, 2.0, 3.0],
+            "s": [1, 2, 3],
+            "sy": ["A", "B", "C"],
+        }
+    )
+    dummy_func(df, price_col="p", size_col="s", symbol_col="sy")
